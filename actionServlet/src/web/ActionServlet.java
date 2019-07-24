0fe5cb28b48181b46634aa19f6b45c07c7f86f67
HuangQitai <861902610@qq.com>
2019-07-24 16:34:26 +0800

    version 2

--- actionServlet/src/web/ActionServlet.java
+++ actionServlet/src/web/ActionServlet.java
@@ -58,22 +58,31 @@ public class ActionServlet{
 	}
 	
 	@RequestMapping("downloadFile")
-		public ResponseEntity<byte[]> download(HttpServletRequest request,
-			       Model model)throws Exception {
-			String filename = request.getParameter("fileName");
-			    //下载文件路径
-			    String path = request.getServletContext().getRealPath("/WEB-INF/file/");
-			    File file = new File(path + File.separator + "download");
-			    HttpHeaders headers = new HttpHeaders(); 
-			    //下载显示的文件名，解决中文名称乱码问题 
-			    String downloadFielName = new String(filename.getBytes("UTF-8"),"iso-8859-1");
-			    //通知浏览器以attachment（下载方式）打开图片
-			    headers.setContentDispositionFormData("attachment", downloadFielName); 
-			    //application/octet-stream ： 二进制流数据（最常见的文件下载）。
-			    headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-			    return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),  
-			        headers, HttpStatus.CREATED); 
-			   }
+    public ResponseEntity<byte[]> DownloadFile(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+         //  接受的是UTF-8
+       req.setCharacterEncoding("utf-8");
+       //获取项目根目录	我试过用来下载pdf、jpg、txt格式的文件，都可以。
+       String path="D:\\InstallPackage\\apache-tomcat-9.0.22-windows-x64\\apache-tomcat-9.0.22\\wtpwebapps\\actionServlet\\WEB-INF\\file\\readme.txt";
+       //获取文件名
+       String filename="readme.txt";
+        File file = null;
+       HttpHeaders headers =null;
+       try {
+          System.out.println(filename);//看看文件名对不对
+          file =new File(path);
+          //请求头
+          //解决文件名乱码，gb2312不行的时候再试试utf-8
+          String fileName1 =new String(filename.getBytes("gb2312"),"iso-8859-1");
+          headers =new HttpHeaders();
+          //通知浏览器以attachment（下载方式）打开文件，我也不知道这种方式是什么
+          headers.setContentDispositionFormData("attachment",fileName1);
+          //application/octet-stream二进制流数据（最常见的文件下载）。
+          headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+       } catch (Exception e) {
+          throw new RuntimeException(e);
+       }
+       return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers, HttpStatus.OK);
+    }
 }
 
 
